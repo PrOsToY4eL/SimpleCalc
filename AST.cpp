7c242a26864ff140a24eab8dbe4c0c252c638a24
PrOsToY4eL <yaroslavslon@gmail.com>
2016-08-26 16:41:50 +0300

    simplify improve

--- AST.cpp
+++ AST.cpp
@@ -9,18 +9,47 @@ void AST::preOrder(const std::shared_ptr<Node> &node)const
 
 void AST::inOrder(const std::shared_ptr<Node> &node)const
 {
-	if (node->left != nullptr) inOrder(node->left);
-	this->print(node);
-	if (node->right != nullptr) inOrder(node->right);
+	if (node == nullptr)
+		return;
+	else
+	{
+		if (node->left != nullptr)
+		{
+			if (node->value->isSymbol() && node->left->value->isSymbol() && (node->value->priority() > node->left->value->priority()))
+			{
+				std::cout << "(";
+				inOrder(node->left);
+				std::cout << ")";
+			}
+			else
+				inOrder(node->left);
+		}
+		this->print(node);
+		if (node->right != nullptr)
+		{
+			if (node->value->isSymbol() && node->right->value->isSymbol() && (node->value->priority() > node->right->value->priority()))
+			{
+				std::cout << "(";
+				inOrder(node->right);
+				std::cout << ")";
+			}
+			else
+				inOrder(node->right);
+		}
+	}
 }
 void AST::postOrder(const std::shared_ptr<Node> &node)const
 {
+	if (node == nullptr)
+		return;
 	if (node->left != nullptr) postOrder(node->left);
 	if (node->right != nullptr) postOrder(node->right);
 	this->print(node);
 }
 void AST::print(const std::shared_ptr<Node> &node)const
 {
+	if (node == nullptr)
+		return;
 	if (node->value->isSymbol())
 		std::cout << node->value->getSymbolValue();
 	else std::cout << node->value->getNumberValue();
@@ -28,27 +57,60 @@ void AST::print(const std::shared_ptr<Node> &node)const
 
 auto AST::copy(const std::shared_ptr<Node> &node)-> std::shared_ptr<Node>const
 {
-	auto nodeCopy{ node };
-	if (node->left != nullptr)
-		nodeCopy->left = copy(node->left);
-	if (node->right != nullptr)
-		nodeCopy->right = copy(node->right);
-	return nodeCopy;
+	if (node != nullptr)
+	{
+		std::shared_ptr<Node> nodeCopy{ node };
+		if (node->left != nullptr)
+			nodeCopy->left = copy(node->left);
+		if (node->right != nullptr)
+			nodeCopy->right = copy(node->right);
+		return nodeCopy;
+	}
+	else
+		return nullptr;
 }
+
 
 void AST::simplify(std::shared_ptr<Node> &root)
 {
-	if (root->value->priority() > root->left->value->priority())
-	{
-		std::shared_ptr<Node> newRoot{ std::move(root->right) };
-		newRoot->parent = nullptr;
-		std::shared_ptr<Node> newLeft{ copy(root) };
-		std::shared_ptr<Node> newRight{ copy(root) };
-		newLeft->right = std::move(newRoot->left);
-		newRight->right = std::move(newRoot->right);
-		newRoot->left = std::move(newLeft);
-		newRoot->right = std::move(newRight);
-		this->root = std::move(newRoot);
-	}
+	if (root->value->isSymbol() && root->left->value->isSymbol())
+		if (root->value->priority() > root->left->value->priority())
+		{
+			std::shared_ptr<Node> newRoot{ copy(root->left) };
+			newRoot->parent.reset();
+			root->left.reset();
+			std::shared_ptr<Node> newLeft{ std::make_shared<Node>(*copy(root)) };
+			std::shared_ptr<Node> newRight{ std::make_shared<Node>(*copy(root)) };
+
+			newLeft->left = copy(newRoot->left);
+			newRoot->left.reset();
+
+			newRight->left = copy(newRoot->right);
+			newRoot->right.reset();
+
+			newRoot->left = newLeft;
+			newRoot->right = newRight;
+			this->root = newRoot;
+		}
+
+	if (root->value->isSymbol() && root->right->value->isSymbol())
+		if (root->value->priority() > root->right->value->priority())
+		{
+			std::shared_ptr<Node> newRoot{ copy(root->right) };
+			newRoot->parent.reset();//improve
+			root->right.reset();
+			std::shared_ptr<Node> newLeft{ std::make_shared<Node>(*copy(root)) };
+			std::shared_ptr<Node> newRight{ std::make_shared<Node>(*copy(root)) };
+
+			newLeft->right = copy(newRoot->left);
+			newRoot->left.reset();
+
+			newRight->right = copy(newRoot->right);
+			newRoot->right.reset();
+
+			newRoot->left = newLeft;
+			newRoot->right = newRight;
+			this->root = newRoot;
+		}
 
 }
